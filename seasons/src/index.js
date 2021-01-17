aecfa42adc06b10fdd12fa9bd80ace0f823f8c1d
Shehan Pramitha <pwshehan@gmail.com>
2021-01-17 12:02:39 +0530

    seasons complete

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+
+import "semantic-ui-css/semantic.min.css";
 
 class App extends React.Component {
   state = { let: null, errorMessage: "" };
@@ -12,7 +15,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -21,7 +24,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading!</div>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 
